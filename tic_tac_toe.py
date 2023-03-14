dc8b86891a53506207e335cd8d5e1800a781b000
PYillo25 <127386464+PYillo25@users.noreply.github.com>
2023-03-14 16:29:53 +0800

    Tic Tac Toe
    Tic Tac Toe: Implement a two player game of Tic Tac Toe.

---
+++ tic_tac_toe.py
@@ -0,0 +1,65 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+game_still_going = True
+winner = None
+current_player = "X"
+
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+def play_game():
+    display_board()
+    while game_still_going:
+        handle_turn(current_player)
+        check_if_game_over()
+        flip_player()
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elif winner == None:
+        print("Tie.")
+
+def handle_turn(player):
+    print(player + "'s turn.")
+    position = input("Choose a position from 1-9: ")
+    valid = False
+    while not valid:
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Choose a position from 1-9: ")
+        position = int(position) - 1
+        if board[position] == "-":
+            valid = True
+        else:
+            print("You can't go there. Try again.")
+    board[position] = player
+    display_board()
+
+def check_if_game_over():
+    check_for_winner()
+    check_for_tie()
+
+def check_for_winner():
+    global winner
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
+
+def check_rows():
+    global game_still_going
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    if row_1
